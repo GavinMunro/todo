ca7cb32e1dacb3f0fd034d793dcbed2656b5c919
gavin <ob1.data@gmail.com>
2019-02-23 23:18:54 +1000

    Add todo routes/endpoints

--- src/main/java/com/todo/controllers/ToDoController.java
+++ src/main/java/com/todo/controllers/ToDoController.java
@@ -3,7 +3,12 @@ package com.todo.controllers;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class ToDoController {
@@ -14,9 +19,23 @@ public class ToDoController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/tasks/validateBrackets")
-	public boolean balBrackets(String exprn) {
+	public boolean validateBrackets(String exprn) {
 		return true;
 	}
 
-	
+    @PostMapping("/todo")
+    public String create_todo(@RequestParam(value = "name", defaultValue = "Job") String name,
+                             @RequestParam(value = "priority", defaultValue = "Medium") String priority) {
+        return String.format(template, name, priority);
+    }
+
+    @GetMapping("/todo_list")
+    public String getlist() {
+        return  "Soon I will return all the todo items from the DB";
+    }
+
+    @GetMapping("/todo/{id}")
+    public String get(@PathVariable(value = "id") int id) {
+        return  "Soon I will return the todo item: " + id;
+    }
 }
