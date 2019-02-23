34601487fa0b999731f319a0ad8087b1557738b4
gavin <ob1.data@gmail.com>
2019-02-24 01:02:06 +1000

    Implement bracket balance

--- src/main/java/com/todo/controllers/ToDoController.java
+++ src/main/java/com/todo/controllers/ToDoController.java
@@ -1,5 +1,6 @@
 package com.todo.controllers;
 
+import java.util.Stack;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +14,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RestController
 public class ToDoController {
 
-	private static final String template = "TODO "
-			+ "  %s, with priority %s!";
+	private static final String template = "TODO: #"
+			+ "%s: %s created at %s";
 
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/tasks/validateBrackets")
-	public boolean validateBrackets(String exprn) {
-		return true;
+	public boolean validateBrackets(@RequestParam(value = "exprn", defaultValue = "") String exprn) {
+            if (exprn.isEmpty())
+                return true;
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < exprn.length(); i++) {
+                char current = exprn.charAt(i);
+                if (current == '{' || current == '(' || current == '[')
+                    { stack.push(current); }
+
+                if (current == '}' || current == ')' || current == ']') {
+                    if (stack.isEmpty())
+                        return false;
+                    char last = stack.peek();
+                    if (   current == '}' && last == '{'
+                        || current == ')' && last == '('
+                        || current == ']' && last == '[' )
+                        stack.pop();
+                    else
+                        return false;
+                }
+            }
+            return stack.isEmpty();
 	}
 
     @PostMapping("/todo")
@@ -30,12 +51,12 @@ public class ToDoController {
     }
 
     @GetMapping("/todo_list")
-    public String getlist() {
+    public String get_list() {
         return  "Soon I will return all the todo items from the DB";
     }
 
     @GetMapping("/todo/{id}")
-    public String get(@PathVariable(value = "id") int id) {
+    public String get_item(@PathVariable(value = "id") int id) {
         return  "Soon I will return the todo item: " + id;
     }
 }
