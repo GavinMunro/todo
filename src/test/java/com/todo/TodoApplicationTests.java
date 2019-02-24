ad20b497078816fe97d5716c3acf2760e76eab3e
gavin <ob1.data@gmail.com>
2019-02-24 19:32:14 +1000

    Add test for balanced brackets

--- src/test/java/com/todo/TodoApplicationTests.java
+++ src/test/java/com/todo/TodoApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.todo.controllers.ToDoController;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +13,18 @@ public class TodoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testBalBraces() {
+		String exp1 = "()[]{}";
+		String exp2 = "[}]{";
+		String exp3 = "dagfd";
+		String exp4 = "{f{gh}[ew4]}()";
+		System.out.println(ToDoController.validateBrackets(exp1));
+		System.out.println(ToDoController.validateBrackets(exp2));
+		System.out.println(ToDoController.validateBrackets(exp3));
+		System.out.println(ToDoController.validateBrackets(exp4));
 	}
 
 }
